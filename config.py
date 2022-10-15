60379e58d547eb4437e677192389cddc31994d4e
Ohad Shapira <ohad.shapira@wiliot.com>
2022-10-15 23:21:24 +0300

    send mail

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 class Config(dict):
     CANNY_EDGE_DETECTOR = True
 
-    INPUT_IMAGE_PATH = "images/pikachu.png"
+    # INPUT_IMAGE_PATH = "images/pikachu.png"
     # INPUT_IMAGE_PATH="images/note.png"
-    # INPUT_IMAGE_PATH="images/einstein.jpg"
+    INPUT_IMAGE_PATH="images/einstein.jpg"
     # INPUT_IMAGE_PATH="images/obama.jpg"
 
-    # OUTPUT_SHAPE=(200, 200)
-    OUTPUT_SHAPE = (400, 400)
+    OUTPUT_SHAPE=(200, 200)
+    # OUTPUT_SHAPE = (400, 400)
     DROP_CONTOURS = False
     CONTOURS_THRESHOLD = 0.00008
 
@@ -15,3 +15,6 @@ class Config(dict):
     VIDEO_INPUT_PATH="video_inputs/"
     VIDEO_OUTPUT_PATH="video_inputs/"
     VIDEO_STEP=0.005
+
+    MAIL_ADDRESS="pyohads@gmail.com"
+    MAIL_PASSWORD="Ohad5656105"
--- utils.py
+++ utils.py
@@ -13,5 +13,43 @@ def images_to_video():
     clip = ImageSequenceClip(image_files, fps=fps)
 
     video_name = '{video_path}{video_name}.mp4'.format(video_path=Config.VIDEO_OUTPUT_PATH,
-                                                        video_name=Config.INPUT_IMAGE_PATH.split("/")[-1][:-4])
+                                                       video_name=Config.INPUT_IMAGE_PATH.split("/")[-1][:-4])
     clip.write_videofile(video_name, verbose=False, logger=None)
+
+
+def send_mail(title, content, attach_file_name=None):
+    from email.mime.text import MIMEText
+    from email.mime.base import MIMEBase
+    from email.mime.multipart import MIMEMultipart
+    from email import encoders
+    import smtplib
+
+    # SMTP server
+    known_errors = ['bdb.BdbQuit', 'invalid device ordinal']
+    if any(known_error in content for known_error in known_errors):
+        return  # dont send mail
+    mail_add = Config.MAIL_ADDRESS  # Your mail
+    s = smtplib.SMTP('smtp.gmail.com:587')
+    s.starttls()
+    s.login(mail_add, Config.MAIL_PASSWORD)  # Your password
+    msg = MIMEMultipart()
+    msg['Subject'] = title
+    text = MIMEText(content)
+    msg.attach(text)
+
+    if attach_file_name:
+        attach_file = open(attach_file_name, 'rb')
+        payload = MIMEBase('application', 'octate-stream')
+        payload.set_payload((attach_file).read())
+        encoders.encode_base64(payload)  # encode the attachment
+
+        # add payload header with filename
+        payload.add_header('Content-Disposition', 'attachment', filename=attach_file_name)
+        msg.attach(payload)
+
+    s.ehlo()
+    try:
+        s.sendmail(mail_add, 'ohadshap@post.bgu.ac.il', msg.as_string())
+    except:
+        print('error sending mail')
+    s.quit()
